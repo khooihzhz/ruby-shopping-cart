352378ef10d4d0788a7aabac65e68fe4ea410426
khooihongzhe <khooihongzhe@gmail.com>
2022-09-02 10:04:07 +0800

    non oop approach

---
+++ lib/checkout.rb
@@ -0,0 +1,69 @@
+# creating class to store Items attributes
+require 'json'
+
+class Item
+  attr_accessor :price, :item_code, :name
+
+  def initialize(item_code, name, price)
+    @item_code = item_code
+    @name = name
+    @price = price
+  end
+end
+
+
+class Checkout
+  attr_accessor :products, :promotional_rules
+
+  def initialize(promotional_rules = nil)
+    @products = []
+    @promotional_rules = JSON.parse(promotional_rules)
+  end
+
+  def scan(item)
+    @products << item
+  end
+
+  def buy_refund
+    promotional_rules.each do |rule|
+      if rule["type"] == "buy"
+        item_count = 0
+        @products.each do | product |
+          # Count number of items
+          if product.item_code == rule["item_code"]
+            item_count += 1
+          end
+        end
+        if item_count >= rule["amount"]
+          @products.each do | product |
+            if product.item_code == rule["item_code"]
+              product.price = rule["new_price"]
+            end
+          end
+        end
+      end
+    end
+  end
+
+  # amount to be eligible for discount
+  def spend_discount
+    total = 0
+    promotional_rules.each do |rule|
+      if rule["type"] == "spend"
+        @products.each do |product|
+          total += product.price
+        end
+        if total >= rule["amount"]
+          total *= 1 - rule["discount"] # discount 10% = 90% of total to be payed
+        end
+      end
+    end
+    total.round(2)
+  end
+
+  def total
+    buy_refund
+    spend_discount
+  end
+end
+
---
+++ test/cart_test.rb
@@ -0,0 +1,58 @@
+require 'minitest/autorun'
+require_relative '../lib/checkout.rb'
+
+PROMOTIONAL_RULES = '
+                  [
+                    {
+                        "type": "spend",
+                        "amount": 60,
+                        "discount": 0.1
+                    },
+                    {
+                        "type": "buy",
+                        "amount": 2,
+                        "item_code": "001",
+                        "new_price": 8.5
+                    }
+                  ]'
+
+class CheckoutTest < Minitest::Test
+  def test_case_1
+    item = Item.new("001", "Red Scarf", 9.25)
+    item_2 = Item.new("002", "Silver cufflinks", 45.00)
+    item_3 = Item.new("003", "Silk Dress", 19.95)
+
+    co = Checkout.new(PROMOTIONAL_RULES)
+    co.scan(item)
+    co.scan(item_2)
+    co.scan(item_3)
+
+    assert co.total == 66.78
+  end
+
+  def test_case_2
+    item = Item.new("001", "Red Scarf", 9.25)
+    item_3 = Item.new("003", "Silk Dress", 19.95)
+
+    co = Checkout.new(PROMOTIONAL_RULES)
+    co.scan(item)
+    co.scan(item_3)
+    co.scan(item)
+
+    assert co.total == 36.95
+  end
+
+  def test_case_3
+    item = Item.new("001", "Red Scarf", 9.25)
+    item_2 = Item.new("002", "Silver cufflinks", 45.00)
+    item_3 = Item.new("003", "Silk Dress", 19.95)
+
+    co = Checkout.new(PROMOTIONAL_RULES)
+    co.scan(item)
+    co.scan(item_2)
+    co.scan(item_3)
+    co.scan(item)
+
+    assert co.total == 73.76
+  end
+end
